b43dfb1dab85ab774cc5700beb90379e2c87058c
Denis Sadowskiy <92denis-s@mail.ru>
2017-09-21 15:46:42 +0300

    changed url in getRate method

--- exchange-rates/src/app/currency.service.ts
+++ exchange-rates/src/app/currency.service.ts
@@ -9,8 +9,11 @@ export class CurrencyService {
   getData() {
     return this.http.get('https://www.nbrb.by/API/ExRates/Currencies');
   }
+  // getRate(){
+  //   return this.http.get('https://www.nbrb.by/API/ExRates/Rates?Periodicity=0');
+  // }
   getRate(){
-    return this.http.get('https://www.nbrb.by/API/ExRates/Rates?Periodicity=0');
+    return this.http.get('http://www.nbrb.by/API/ExRates/Rates/Dynamics/190?startDate=2016-6-1&endDate=2016-6-30');
   }
 
 }
--- exchange-rates/src/app/rate.ts
+++ exchange-rates/src/app/rate.ts
@@ -1,8 +1,5 @@
 export class Rate {
     private Cur_ID: number;
     private Date : Date;
-    private Cur_Abbreviation: string;
-    private Cur_Scale: number;
-    private Cur_Name: string;
     private Cur_OfficialRate: number;
 }
