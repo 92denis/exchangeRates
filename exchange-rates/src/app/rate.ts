1d3e0f906198890e83b990a0b555c7cdef538c4f
Denis Sadowskiy <92denis-s@mail.ru>
2017-09-21 13:25:42 +0300

    created class Rate

---
+++ exchange-rates/src/app/rate.ts
@@ -0,0 +1,8 @@
+export class Rate {
+    private Cur_ID: number;
+    private Date : Date;
+    private Cur_Abbreviation: string;
+    private Cur_Scale: number;
+    private Cur_Name: string;
+    private Cur_OfficialRate: number;
+}
