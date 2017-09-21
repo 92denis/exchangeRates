ea4dbfc65bd29679769289d4d12c2acb05bd1ccf
Denis Sadowskiy <92denis-s@mail.ru>
2017-09-21 12:49:46 +0300

    create currencyData array

--- exchange-rates/src/app/app.component.ts
+++ exchange-rates/src/app/app.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
-import { CurrencyService} from './currency.service';
+import { Component, OnInit } from '@angular/core';
+import { CurrencyService } from './currency.service';
+import { Currency } from './currency'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  currencies = ["USD","EUR","RUR"];
 
-  constructor(private currencyService: CurrencyService){}
-  
- ngOnInit(){
-      
-   
- }
+export class AppComponent implements OnInit {
+
+  currencies = ["USD", "EUR", "RUR"];
+  currenciesData: Currency[] = [];
+
+  constructor(private currencyService: CurrencyService) { }
+
+  ngOnInit(): void {
+
+    this.currencyService.getData().subscribe((data) => this.currenciesData = data.json());
+    console.log();
+  }
 }
--- exchange-rates/src/app/currency.service.ts
+++ exchange-rates/src/app/currency.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
-import {Http} from '@angular/http';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class CurrencyService {
 
-  constructor(private http: Http){ }
-  
- getData(){
-     return this.http.get('http://www.nbrb.by/API/ExRates/Currencies')
- }
+  constructor(private http: Http) { }
+
+  getData() {
+    return this.http.get('http://www.nbrb.by/API/ExRates/Currencies');
+  }
 
 }
