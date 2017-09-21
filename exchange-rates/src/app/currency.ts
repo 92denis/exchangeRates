8b83f4e7d22369c18adcca658be6bd42350686d4
Denis Sadowskiy <92denis-s@mail.ru>
2017-09-21 12:13:37 +0300

    create class currency

---
+++ exchange-rates/src/app/currency.ts
@@ -0,0 +1,19 @@
+export class Currency {
+    private Cur_ID: number; 
+    private Cur_ParentID: number;
+    private Cur_Code: number;
+    private Cur_Abbreviation:string;
+    private Cur_Name:string;
+    private Cur_Name_Bel:string;
+    private Cur_Name_Eng:string;
+    private Cur_QuotName:string;
+    private Cur_QuotName_Eng:string;
+    private Cur_NameMulti:string;
+    private Cur_Name_BelMulti:string;
+    private Cur_Name_EngMulti:string;
+    private Cur_Scale: number;
+    private Cur_Periodicity: boolean;
+    private Cur_DateStart :Date;
+    private Cur_DateEnd: Date;
+}
+
