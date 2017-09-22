e02d4899a797117734f50a59e1c180c045d16837
Denis Sadowskiy <92denis-s@mail.ru>
2017-09-22 11:36:18 +0300

    RateComponent: renamed variables in method ngOnChange

--- exchange-rates/src/app/rate/rate.component.ts
+++ exchange-rates/src/app/rate/rate.component.ts
@@ -16,17 +16,16 @@ export class RateComponent implements OnInit {
   endDate: Date;
   rates: Rate[] = [];
   constructor(private currencyService: CurrencyService, private dateAdapter: DateAdapter<Date>) {
-    this.dateAdapter.setLocale('nl');
+    this.dateAdapter.setLocale('en');
     this.startDate = new Date();
     this.endDate = new Date();
-
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    let startD = this.startDate.getFullYear() + '-' + (this.startDate.getMonth() + 1) + '-' + this.startDate.getDate();
-    let endD = this.endDate.getFullYear() + '-' + (this.endDate.getMonth() + 1) + '-' + this.endDate.getDate();
+    let start = this.startDate.getFullYear() + '-' + (this.startDate.getMonth() + 1) + '-' + this.startDate.getDate();
+    let end = this.endDate.getFullYear() + '-' + (this.endDate.getMonth() + 1) + '-' + this.endDate.getDate();
     this.cur = changes.CurId.currentValue;
-    this.currencyService.getRate(this.cur, startD, endD).subscribe((data) => {
+    this.currencyService.getRate(this.cur, start, end).subscribe((data) => {
       this.rates = data.json()
     });
 
