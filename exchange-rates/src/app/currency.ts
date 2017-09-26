f18b0b2a49433c358c9197aa81e21827880a41f7
Denis Sadowskiy <92denis-s@mail.ru>
2017-09-27 02:17:41 +0300

    public property

--- exchange-rates/src/app/currency.ts
+++ exchange-rates/src/app/currency.ts
@@ -1,19 +1,19 @@
 export class Currency {
-    private Cur_ID: number; 
-    private Cur_ParentID: number;
-    private Cur_Code: number;
-    private Cur_Abbreviation:string;
-    private Cur_Name:string;
-    private Cur_Name_Bel:string;
-    private Cur_Name_Eng:string;
-    private Cur_QuotName:string;
-    private Cur_QuotName_Eng:string;
-    private Cur_NameMulti:string;
-    private Cur_Name_BelMulti:string;
-    private Cur_Name_EngMulti:string;
-    private Cur_Scale: number;
-    private Cur_Periodicity: boolean;
-    private Cur_DateStart :Date;
-    private Cur_DateEnd: Date;
+    public Cur_ID: number;
+    public Cur_ParentID: number;
+    public Cur_Code: number;
+    public Cur_Abbreviation: string;
+    public Cur_Name: string;
+    public Cur_Name_Bel: string;
+    public Cur_Name_Eng: string;
+    public Cur_QuotName: string;
+    public Cur_QuotName_Eng: string;
+    public Cur_NameMulti: string;
+    public Cur_Name_BelMulti: string;
+    public Cur_Name_EngMulti: string;
+    public Cur_Scale: number;
+    public Cur_Periodicity: boolean;
+    public Cur_DateStart: string;
+    public Cur_DateEnd: string;
 }
 
