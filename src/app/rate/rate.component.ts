5d72df56a41814b241337239022cbbf7e276bea6
Denis Sadowskiy <sadowskiydenis@gmail.com>
2017-10-04 22:35:09 +0300

    created chart with 3 currencies

--- src/app/rate/rate.component.ts
+++ src/app/rate/rate.component.ts
@@ -16,12 +16,22 @@ export class RateComponent implements OnInit {
   @Input() endDate: Date;
 
   rates: Rate[] = [];
-
+  public usa: number[] = [];
+  public rus: number[] = [];
+  public euro: number[] = [];
   public OfficialRate: number[] = [];
   public dates: string[] = [];
+  public datesRate: string[] = [];
   public lineChartData: Array<any> = [
     { data: [], label: undefined },
   ];
+  public lineChar: Array<any> = [
+    { data: [], label: undefined },
+    { data: [], label: undefined },
+    { data: [], label: undefined }
+  ];
+
+
   // lineChart
 
   public lineChartOptions: any = {
@@ -34,13 +44,32 @@ export class RateComponent implements OnInit {
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
       pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // dark grey
+      backgroundColor: 'rgba(77,83,96,0.2)',
+      borderColor: 'rgba(77,83,96,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(77,83,96,1)'
+    },
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
     }
   ];
   public lineChartLegend: boolean = true;
   public lineChartType: string = 'line';
+  private start: string;
+  private end: string;
 
   constructor(private currencyService: CurrencyService, private dateAdapter: DateAdapter<Date>) {
     this.dateAdapter.setLocale('en');
+
   }
 
   typesOfChart: Array<any> = [
@@ -52,15 +81,10 @@ export class RateComponent implements OnInit {
     if (!this.currency || !this.startDate || !this.endDate) {
       return;
     }
-    let start = this.startDate.getFullYear() + '-' + (this.startDate.getMonth() + 1) + '-' + this.startDate.getDate();
-    let end = this.endDate.getFullYear() + '-' + (this.endDate.getMonth() + 1) + '-' + this.endDate.getDate();
-    this.getRates(this.currency.Cur_ID, start, end);
-    this.lineChartData[0] =
-    { data: this.OfficialRate, label: this.currency.Cur_Name };
-  }
+    this.start = this.startDate.getFullYear() + '-' + (this.startDate.getMonth() + 1) + '-' + this.startDate.getDate();
+    this.end = this.endDate.getFullYear() + '-' + (this.endDate.getMonth() + 1) + '-' + this.endDate.getDate();
 
-  getRates(id, start, end) {
-    this.currencyService.getRate(id, start, end).subscribe((data) => {
+    this.currencyService.getRate(this.currency.Cur_ID, this.start, this.end).subscribe((data) => {
       this.rates = data.json();
 
       for (let i = 1; i < this.rates.length; i++) {
@@ -75,12 +99,36 @@ export class RateComponent implements OnInit {
 
 
       this.OfficialRate = this.rates.map(x => x.Cur_OfficialRate);
-     
-        return this.OfficialRate
+      this.lineChartData[0] =
+        { data: this.OfficialRate, label: this.currency.Cur_Name };
+    });
+  }
+
+  ngOnInit() {
+    this.currencyService.getRate(145, this.start, this.end).subscribe((data) => {
+      this.rates = data.json();
+      this.datesRate = this.rates.map(x => x.Date.toString().substring(0, 10));
+      this.usa = this.rates.map(x => x.Cur_OfficialRate);
+      this.lineChar[0] = { data: this.usa, label: 'USA' };
+
+    });
+    this.currencyService.getRate(298, this.start, this.end).subscribe((data) => {
+      this.rates = data.json();
+
+      this.rus = this.rates.map(x => x.Cur_OfficialRate);
+      this.datesRate = this.rates.map(x => x.Date.toString().substring(0, 10));
+      this.lineChar[1] =
+        { data: this.rus, label: 'RUB' };
+
+    });
+    this.currencyService.getRate(292, this.start, this.end).subscribe((data) => {
+      this.rates = data.json();
+      this.datesRate = this.rates.map(x => x.Date.toString().substring(0, 10));
+      this.euro = this.rates.map(x => x.Cur_OfficialRate);
+      this.lineChar[2] =
+        { data: this.euro, label: 'EUR' };
     });
 
-  }
-  ngOnInit() {
   }
 
   // events
