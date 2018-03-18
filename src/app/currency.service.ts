dcf5c3a00802fa5e32aa9c8988af276d62cb140d
Denis Sadowskiy <sadowskiydenis@gmail.com>
2018-03-18 23:04:12 +0300

    changed warning tslint

--- src/app/currency.service.ts
+++ src/app/currency.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import { Rate } from "./rate";
+import { Rate } from './rate';
 import { Currency } from './currency';
-import { Observable } from "rxjs/Observable";
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -12,25 +12,25 @@ export class CurrencyService {
   constructor(private http: Http) { }
 
   getCurrency(): Observable<Currency[]> {
-    let observable = this.http.get('https://www.nbrb.by/API/ExRates/Currencies');
-    let mappedObservable = observable.map((response) => {
+    const observable = this.http.get('http://www.nbrb.by/API/ExRates/Currencies');
+    const mappedObservable = observable.map((response) => {
       this.currencies = <Currency[]>response.json(); return this.currencies;
     });
     return mappedObservable;
   }
 
   getRate(id: number, startDate: Date, endDate: Date): Observable<Rate[]> {
-    let start = startDate.getFullYear() + '-' + (startDate.getMonth() + 1) + '-' + startDate.getDate();
-    let end = endDate.getFullYear() + '-' + (endDate.getMonth() + 1) + '-' + endDate.getDate();
+    const start = startDate.getFullYear() + '-' + (startDate.getMonth() + 1) + '-' + startDate.getDate();
+    const end = endDate.getFullYear() + '-' + (endDate.getMonth() + 1) + '-' + endDate.getDate();
 
-    let observable = this.http.get('https://www.nbrb.by/API/ExRates/Rates/Dynamics/' + id + '?startDate=' + start + '&endDate=' + end);
-    let mappedObservable = observable.map((response) => {
+    const observable = this.http.get('https://www.nbrb.by/API/ExRates/Rates/Dynamics/' + id + '?startDate=' + start + '&endDate=' + end);
+    const mappedObservable = observable.map((response) => {
       this.rates = <Rate[]>response.json();
 
 
       for (let i = 0; i < this.rates.length; i++) {
         this.rates[i].Date = this.rates[i].Date.substring(0, 10);
-        if (i != 0) {
+        if (i !== 0) {
           this.rates[i].delta = +(this.rates[i].Cur_OfficialRate - this.rates[i - 1].Cur_OfficialRate).toFixed(4);
         }
       }
